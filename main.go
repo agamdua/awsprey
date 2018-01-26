842268a6e115968bda0037703b3352cdb8e0b4e5
Agam Dua <agam.dua@gc.com>
2018-01-26 13:25:58 -0500

    Added: subcommand

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
@@ -19,41 +21,67 @@ func main() {
 
 	ec2svc := ec2.New(cfg)
 
-	dryRun := false
-	tagFilterService := "tag:service"
-	tagFilterEnvironment := "tag:environment"
+	listCommand := flag.NewFlagSet("list", flag.ExitOnError)
 
-	serviceFilters := []ec2.Filter{
-		{
-			Name:   &tagFilterService,
-			Values: []string{""},
-		},
-		{
-			Name:   &tagFilterEnvironment,
-			Values: []string{""},
-		},
+	if len(os.Args) <= 1 {
+		fmt.Println(
+			`Oh no - you forgot to specify what exactly to do!
+			
+Must provide a subcommand out of the following options: list
+
+Example usage:
+	$ boxcar list web:staging
+`)
+
+		os.Exit(1)
 	}
 
-	input := ec2.DescribeInstancesInput{
-		DryRun:  &dryRun,
-		Filters: serviceFilters,
+	switch os.Args[1] {
+	case "list":
+		listCommand.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
 
-	req := ec2svc.DescribeInstancesRequest(&input)
-	resp, err := req.Send()
-	if err != nil {
-		panic(err)
-	}
+	if listCommand.Parsed() {
+		dryRun := false
+		tagFilterService := "tag:service"
+		tagFilterEnvironment := "tag:environment"
 
-	// TODO: when the whole function is refactored out from `main` we should
-	//return the list of instance names
-	var instanceNames []string
+		serviceFilters := []ec2.Filter{
+			{
+				Name:   &tagFilterService,
+				Values: []string{"web"},
+			},
+			{
+				Name:   &tagFilterEnvironment,
+				Values: []string{"staging"},
+			},
+		}
 
-	for _, instance := range resp.Reservations[0].Instances {
-		for _, tag := range instance.Tags {
-			if *tag.Key == "Name" {
-				instanceNames = append(instanceNames, *tag.Value)
-				fmt.Println(*tag.Value)
+		input := ec2.DescribeInstancesInput{
+			DryRun:  &dryRun,
+			Filters: serviceFilters,
+		}
+
+		req := ec2svc.DescribeInstancesRequest(&input)
+		resp, err := req.Send()
+
+		if err != nil {
+			panic(err)
+		}
+
+		// TODO: when the whole function is refactored out from `main` we should
+		//return the list of instance names
+		var instanceNames []string
+
+		for _, instance := range resp.Reservations[0].Instances {
+			for _, tag := range instance.Tags {
+				if *tag.Key == "Name" {
+					instanceNames = append(instanceNames, *tag.Value)
+					fmt.Println(*tag.Value)
+				}
 			}
 		}
 	}
